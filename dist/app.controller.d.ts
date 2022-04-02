c01b91cce32a98552ca3fc032b002ed9c4afa1a8
Riccardo Barbatti <barbatti.riccardo@gmail.com>
2022-04-02 11:37:34 +0200

    Delete dist directory

--- dist/app.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { PusherService } from "./pusher/pusher.service";
-export declare class AppController {
-    private pusherService;
-    constructor(pusherService: PusherService);
-    messages(username: string, message: string): Promise<any[]>;
-    getMessage(): void;
-}
